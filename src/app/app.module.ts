14113def0b6c0fe8a3b41d074a4b79636ccdf30f
Hamada <defjamvk@gmail.com>
2022-02-20 04:45:02 +0100

    add a table form grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,8 @@ import { HeroesComponent } from './hero/heroes/heroes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero/heroes/heroes.component.ts
+++ src/app/hero/heroes/heroes.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { ColDef } from 'ag-grid-community';
+
 import { HeroService } from '../hero.service';
 import { Hero } from '../model/hero';
+
+
 
 
 @Component({
@@ -14,7 +19,7 @@ export class HeroesComponent implements OnInit {
   public versions: string = '';
   public arrHeroes: Array<Hero> = [];
 
-  constructor(public heroes: HeroService) { }
+  constructor(public heroes: HeroService) {}
 
   ngOnInit(): void {
 
@@ -29,7 +34,22 @@ export class HeroesComponent implements OnInit {
           info[item].key,
           info[item].name)
         this.arrHeroes.push(hero);
+
       }
+      this.rowData = this.getHeroes(this.arrHeroes)
     })
   }
+
+  columnDefs: ColDef[] = [
+        { field: 'id' , sortable: true, filter: true },
+        { field: 'name' , sortable: true, filter: true },
+        { field: 'title', sortable: true, filter: true },
+        { field: 'key', sortable: true, filter: true }
+    ];
+
+ rowData: Observable<any[]> | undefined;
+
+
+ getHeroes(arrHeroes: any) { return of(arrHeroes); }
+
 }
